2de63b3962ff77870ecb113dff63066cda7ef459
David Whipple <dwhipp88@gmail.com>
2023-08-11 15:45:36 -0600

    FUNCTIONALITY UPDATE: added clear list logic

--- assets/index.js
+++ assets/index.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     $('#input').change(function () {
         let input = $(this).val();
         $('ul').append(
-            '<li>' +
-                input +
-                ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>'
+            '<li>' + input + ' <i class="fas fa-check"></i></li>'
         );
+        localStorage.setItem(input, input);
         $(this).val('');
     });
 
-    $('ul').on('click', '.fa-trash', function () {
-        $(this).parent('li').fadeOut(200);
+    $('.clear-list-btn').click(function () {
+        localStorage.clear();
+        console.log('clear list: ', $('#item-list').empty());
     });
 
     $('ul').on('click', '.fa-check', function () {
