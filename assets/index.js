93fa08ba51d6bd4522134d9bb3701005c38df3d9
David Whipple <dwhipp88@gmail.com>
2023-08-17 21:00:46 -0600

    added functionality, app now loads items from localStorage

--- assets/index.js
+++ assets/index.js
@@ -10,10 +10,40 @@ $(document).ready(function () {
 
     $('.clear-list-btn').click(function () {
         localStorage.clear();
-        console.log('clear list: ', $('#item-list').empty());
+        $('#item-list').empty();
+    });
+
+    let prices = [];
+
+    $('.price-input').change(function () {
+        $('.total-cost').val('');
+
+        let price = parseFloat($(this).val());
+
+        prices.push(price);
+        console.log(prices);
+
+        let sum = 0;
+
+        for (let i = 0; i < prices.length; i++) {
+            sum += prices[i];
+            console.log('sum: ', typeof sum);
+        }
+
+        $('.total-cost').text(sum);
     });
 
     $('ul').on('click', '.fa-check', function () {
         $(this).parent('li').toggleClass('checked');
     });
+
+    let keys = Object.keys(localStorage);
+    console.log(keys);
+
+    for (let j = 0; j < keys.length; j++) {
+        console.log(keys[j]);
+        $('ul').append(
+            '<li>' + keys[j] + '<i class="fas fa-check"></i></li>'
+        );
+    }
 });
