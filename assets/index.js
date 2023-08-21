9869b0907cd1a9db417b9e725ca3d319fd3b405e
David Whipple <dwhipp88@gmail.com>
2023-08-21 16:18:39 -0600

    setup new form to capture user input and store it in items array

--- assets/index.js
+++ assets/index.js
@@ -1,44 +1,64 @@
 $(document).ready(function () {
-    $('#input').change(function () {
-        let input = $(this).val();
-        $('ul').append(
-            '<li>' + input + ' <i class="fas fa-check"></i></li>'
-        );
-        localStorage.setItem(input, input);
-        $(this).val('');
+    // $('#item-name').change(function () {
+    //     let itemName = $(this).val();
+    //     let itemPrice = $('#item-price').val();
+    //     console.log(itemPrice);
+
+    //     $('ul').append(
+    //         '<li>' + itemName + ' <i class="fas fa-check"></i></li>'
+    //     );
+    //     localStorage.setItem(itemName, itemName);
+    //     $(this).val('');
+    // });
+
+    // $('.clear-list-btn').click(function () {
+    //     localStorage.clear();
+    //     $('#item-list').empty();
+    // });
+    let items = [];
+
+    $('form#content-form').submit(function (e) {
+        e.preventDefault();
+
+        let itemName = $('#form-item-name').val();
+        let itemPrice = $('#form-item-price').val();
+        console.log('Form items: ' + itemName + ' ' + itemPrice);
+        let fields = {
+            name: itemName,
+            price: itemPrice,
+            id: Date.now(),
+        };
+        console.log('Fields: ', fields);
+        items.push(fields);
+        console.log('items: ', items);
     });
 
-    $('.clear-list-btn').click(function () {
-        localStorage.clear();
-        $('#item-list').empty();
-    });
+    // let prices = [];
 
-    let prices = [];
+    // $('.price-input').change(function () {
+    //     $('.total-cost').val('');
 
-    $('.price-input').change(function () {
-        $('.total-cost').val('');
+    //     let price = parseFloat($(this).val());
 
-        let price = parseFloat($(this).val());
+    //     prices.push(price);
+    //     console.log(prices);
 
-        prices.push(price);
-        console.log(prices);
+    //     let sum = 0;
 
-        let sum = 0;
+    //     for (let i = 0; i < prices.length; i++) {
+    //         sum += prices[i];
+    //         console.log('sum: ', typeof sum);
+    //     }
 
-        for (let i = 0; i < prices.length; i++) {
-            sum += prices[i];
-            console.log('sum: ', typeof sum);
-        }
-
-        $('.total-cost').text(sum);
-    });
+    //     $('.total-cost').text(sum);
+    // });
 
     $('ul').on('click', '.fa-check', function () {
         $(this).parent('li').toggleClass('checked');
     });
 
     let keys = Object.keys(localStorage);
-    console.log(keys);
+    // console.log('Keys: ', keys);
 
     for (let j = 0; j < keys.length; j++) {
         console.log(keys[j]);
